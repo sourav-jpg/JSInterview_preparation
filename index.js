1940185d6ae3a49ef1ea6b0ade3ef3c9e3b2d940
sourav-jpg <souravpadmaraj6@gmail.com>
2023-02-15 15:26:48 +0530

    call , apply, bind

--- index.js
+++ index.js
@@ -179,3 +179,25 @@
 // const arr = [3,5,1];
 // const result = arr.sort((a,b)=>(a<b ? -1:1))
 // console.log(arr,result);
+
+//--------------------------------------------------------------------
+
+//call apply and bind
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+console.log(greeting + ' '+ this.firstName + ' ' + this.lastName);
+}
+
+// say.call(person1, 'Hello'); // Hello Jon Kuperman
+// say.call(person2, 'Hello'); // Hello Kelly King
+
+// say.apply(person1,['Maga']);
+// say.apply(person2,['Maga']);
+
+// var sayHelloJon = say.bind(person1);
+// var sayHelloKelly = say.bind(person2);
+
+// sayHelloJon(); // Hello Jon Kuperman
+// sayHelloKelly(); // Hello Kelly King
